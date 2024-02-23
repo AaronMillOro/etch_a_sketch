26fa475417924ec4e95f70f125e8c1c4acec7a24
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-02-23 02:09:59 +0100

    :sparkles: coloring of cells when hovering

--- script.js
+++ script.js
@@ -16,9 +16,15 @@ function createGrid(nbPixels){
     for (let y = 1; y <= nbPixels; y++){
         for (let x = 1; x <= nbPixels; x++){
             const pixel = document.createElement('div')
+
             pixel.style['width'] = Math.floor(sketch.offsetWidth/nbPixels)+'px'
             pixel.style['height'] = Math.floor(sketch.offsetHeight/nbPixels)+'px'
             pixel.classList.add('pixel')
+            pixel.addEventListener('mouseover', () => {
+                pixel.style['background-color'] = `rgb(${setRandomValue()}, ${setRandomValue()}, ${setRandomValue()})`
+                // for reseting the color after a delay
+                setTimeout(() => {pixel.style['background-color'] = '#e0dede'}, '450')
+            })
             sketch.appendChild(pixel)
         }
     }
@@ -29,4 +35,9 @@ function resetGrid(){
     for (let i = 0; i < pixels.length; i++){
         sketch.removeChild(pixels[i])
     }
+}
+
+// random value between 0 to 255
+function setRandomValue(){
+    return Math.floor(Math.random() * 255)
 }
