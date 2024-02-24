d47edf4ca2654813186298963df2f64f772e42d2
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-02-25 00:32:20 +0100

    :zap: improved performance - re-structuration on function to create individual pixels - modification on loops to create document fragments and append only one child on each grid creation

--- script.js
+++ script.js
@@ -2,58 +2,61 @@ const sketch = document.getElementById('sketch')
 const btn = document.getElementById('new-grid')
 let initialGrid = 16
 
+createGrid(initialGrid)
+
 window.onresize = () => {
     const currentGrid = document.querySelectorAll('.pixel')
-    resetGrid()
+    sketch.innerHTML = '' // reset grid
     createGrid(currentGrid.length ** 0.5)
 }
-
-createGrid(initialGrid)
 
 btn.addEventListener('click', () => {
     do { 
         initialGrid = prompt('Hit a number (from 1 to 100)')
     } while (isNaN(initialGrid) || initialGrid > 100 || initialGrid <= 0)
-    resetGrid()
+    sketch.innerHTML = '' // reset grid
     createGrid(initialGrid)
 })
 
 function createGrid(nbPixels){
-    for (let y = 1; y <= nbPixels; y++){
-        for (let x = 1; x <= nbPixels; x++){
-            const pixel = document.createElement('div')
-
-            pixel.style['width'] = Math.floor(sketch.offsetWidth/nbPixels)+'px'
-            pixel.style['height'] = Math.floor(sketch.offsetHeight/nbPixels)+'px'
-            pixel.classList.add('pixel')
-            pixel.style['cursor'] = 'grab'
-            pixel.addEventListener('mouseover', () => {
-                if(isNaN(pixel.style['background-color'])){
-                    const color = pixel.style['background-color']
-                    if (!color.includes('0.9')){
-                        let darkness = +color.split(', ')[3].slice(0,3)
-                        if (darkness < 1){ 
-                            darkness += 0.1
-                            const nextColor =  color.substring(0, color.length-4) + darkness.toFixed(1) + ')'
-                            pixel.style['background-color'] = nextColor
-                        }
-                    }
-                } else {
-                    pixel.style['background-color'] = `rgba(${setRandomValue()}, ${setRandomValue()}, ${setRandomValue()}, 0.1)`
-                }
-            })
-            sketch.appendChild(pixel)
-        }
+    const fragment =  document.createDocumentFragment()
+    // to get a squared grid
+    for (let y = 1; y <= nbPixels * nbPixels; y++){
+        const pixel = createPixel(nbPixels)
+        fragment.appendChild(pixel)
     }
-}
-
-function resetGrid(){
-    const pixels = document.querySelectorAll('.pixel')
-    for (let i = 0; i < pixels.length; i++){
-        sketch.removeChild(pixels[i])
-    }
+    sketch.appendChild(fragment)
 }
 
 function setRandomValue(){
     return Math.floor(Math.random() * 255)
+}
+
+function createPixel(nbPixels){
+    // the number of pixel is to claculate the width and height in function of the  size of the sketch zone
+    const pixel = document.createElement('div')
+    pixel.style['width'] = Math.floor(sketch.offsetWidth/nbPixels)+'px'
+    pixel.style['height'] = Math.floor(sketch.offsetHeight/nbPixels)+'px'
+    pixel.classList.add('pixel')
+    pixel.style['cursor'] = 'grab'
+    pixel.addEventListener('mouseover', () => {
+        if(isNaN(pixel.style['background-color'])){
+            const color = pixel.style['background-color']
+            intensityColorDarkness(color)
+        } else {
+            pixel.style['background-color'] = `rgba(${setRandomValue()}, ${setRandomValue()}, ${setRandomValue()}, 0.1)`
+        }
+    })
+    return pixel
+}
+
+function intensityColorDarkness(color){
+    if (!color.includes('0.9')){
+        let darkness = +color.split(', ')[3].slice(0,3)
+        if (darkness < 1){ 
+            darkness += 0.1
+            const nextColor =  color.substring(0, color.length-4) + darkness.toFixed(1) + ')'
+            pixel.style['background-color'] = nextColor
+        }
+    }
 }
